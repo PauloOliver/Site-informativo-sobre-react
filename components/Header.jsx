4dd0851d9642a768d368ab11499bba293924f210
PauloOliver <63667729+PauloOliver@users.noreply.github.com>
2025-03-15 13:01:13 -0300

    commit project

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <>
+            <header className="header">
+                <nav className="headerNav">
+                    <img src="./globe.png"></img>
+                    <h1> diário de viagem </h1>
+                </nav>
+            </header>
+        </>
+    )
+}
